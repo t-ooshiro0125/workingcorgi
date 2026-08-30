61bac5d93a0728a807827acb017f3c2e28897d74
t-ooshiro0125 <corgi.shim0125@gmail.com>
2026-08-30 11:26:40 +0000

    feat: Notesのカテゴリ別OGP画像を設定

--- src/config/noteCategoryOgps.ts
+++ src/config/noteCategoryOgps.ts
@@ -1,14 +1,21 @@
 import type { NoteCategory } from "./noteCategories";
 
 type NoteCategoryOgp = {
-  image: string;
-  imageAlt: string;
+  readonly image: string;
+  readonly imageAlt: string;
 };
 
-export const noteCategoryOgps: Partial<Record<NoteCategory, NoteCategoryOgp>> =
-  {
-    tech: {
-      image: "/og-image-note-tech.png",
-      imageAlt: "ノートパソコンで作業するコーギーと TECH の文字",
-    },
-  };
+export const noteCategoryOgps = {
+  tech: {
+    image: "/og-image-note-tech.png",
+    imageAlt: "ノートパソコンで作業するコーギーと TECH の文字",
+  },
+  note: {
+    image: "/og-image-note-note.png",
+    imageAlt: "ノートに書き込むコーギーと NOTE の文字",
+  },
+  devlog: {
+    image: "/og-image-note-devlog.png",
+    imageAlt: "ターミナルに表示されたコーギーと DEVLOG の文字",
+  },
+} as const satisfies Record<NoteCategory, NoteCategoryOgp>;
